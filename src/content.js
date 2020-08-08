845e29d243300fb242b03e1a7dc88f1b6e2c5bf7
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-08 19:03:33 +0900

    search input and container

--- src/content.js
+++ src/content.js
@@ -10,11 +10,66 @@ function Main() {
   const [modalIsOpen, openModal, closeModal] = useSwitch();
   port.postMessage({ message: "Hello, backgroundJs! -- from contentJs" });
   return (
-    <ReactModal isOpen={true} onRequestClose={closeModal}>
-      <div>Test</div>
-    </ReactModal>
+    <Modal isOpen={true} onRequestClose={closeModal}>
+            <div className="flexbox flexbox-direction-column flexbox-grow-1 radius-small border-width-normal border-solid border-color-shade-013 background-shade-003 overflow-hidden">
+                <div className="flexbox flexbox-align-center  padding-left-small padding-right-tiny" style={{height: 44}} > 
+                    <div className="flexbox flexbox-centered padding-left-smaller padding-right-medium shade-087">
+                        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" style={{stroke: "currentColor", strokeWidth: 4, height: 16, width: 16, display: "block"}}>
+                            <g fill="none">
+                                <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="flexbox-grow-1">
+                        <input type="text" className="shade-087 font-size-medium font-weight-medium line-height-medium overflow-ellipsis no-border no-outline no-box-shadow full-width background-none"/>
+                    </div>
+                </div>  
+            </div>
+    </Modal>
   );
 }
+
+function Modal({ isOpen, onRequestClose, children }) {
+    return (
+      <ReactModal
+        id="act-hub-modal-root"
+        // className="padding-horizontal-larger"
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        style={{
+          overlay: {
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            backgroundColor: "rgba(255, 255, 255, .0)",
+            zIndex: 100000
+          },
+          content: {
+            position: null,
+            padding: 0,
+            top: null,
+            right: null,
+            left: null,
+            bottom: null,
+            marginTop: "20vh",
+            marginLeft: 24,
+            marginRight: 24,
+            maxWidth: 683,
+            flexGrow: 1,
+            border: 0,
+            borderRadius: 4,
+            boxShadow: "rgba(29, 17, 17, 0.15) 0px 5px 12px"
+          }
+        }}
+      >
+        {children}
+      </ReactModal>
+    );
+  }
+  
+
+
 
 const app = document.createElement("div");
 app.id = "act-hub-extension-root";
