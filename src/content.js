015432b0e9624965514b72d7ca8d2503746d1f74
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-08 16:13:30 +0900

    RactModal

--- src/content.js
+++ src/content.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactModal from "react-modal";
 import "./css/content.css";
-import usePort from './hooks/chrome/usePort';
+import usePort from "./hooks/chrome/usePort";
+import useSwitch from "./hooks/useSwitch";
 
 function Main() {
-    const port = usePort('ActHub')
-    port.postMessage({message: 'Hello, backgroundJs! -- from contentJs'})
-    return <div></div>
+  const port = usePort("ActHub");
+  const [modalIsOpen, openModal, closeModal] = useSwitch();
+  port.postMessage({ message: "Hello, backgroundJs! -- from contentJs" });
+  return (
+    <ReactModal isOpen={true} onRequestClose={closeModal}>
+      <div>Test</div>
+    </ReactModal>
+  );
 }
 
-const app = document.createElement('div');
+const app = document.createElement("div");
 app.id = "act-hub-extension-root";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
