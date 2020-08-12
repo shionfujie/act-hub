56c776bc9dccaba5b2d88b3311a8b928a5640740
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-12 17:07:07 +0900

    separate SearchInput

---
+++ src/components/SearchInput.js
@@ -0,0 +1,37 @@
+import React from "react";
+import useFocusCallback from "../hooks/useFocusCallback";
+
+export default function SearchInput() {
+  const focusCallback = useFocusCallback();
+  return (
+    <div
+      className="flexbox flexbox-align-center  padding-left-small padding-right-tiny"
+      style={{ height: 44 }}
+    >
+      <div className="flexbox flexbox-centered padding-left-smaller padding-right-medium shade-087">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{
+            stroke: "currentColor",
+            strokeWidth: 4,
+            height: 16,
+            width: 16,
+            display: "block"
+          }}
+        >
+          <g fill="none">
+            <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
+          </g>
+        </svg>
+      </div>
+      <div class="flexbox-grow-1">
+        <input
+          type="text"
+          className="shade-087 font-size-medium font-weight-medium line-height-medium overflow-ellipsis no-border no-outline no-box-shadow full-width background-none"
+          ref={focusCallback}
+        />
+      </div>
+    </div>
+  );
+}
--- src/content.js
+++ src/content.js
@@ -5,9 +5,9 @@ import ReactModal from "react-modal";
 import "./css/content.css";
 import usePort from "./hooks/chrome/usePort";
 import useSwitch from "./hooks/useSwitch";
-import useFocusCallback from "./hooks/useFocusCallback";
 import useDocumentKeydown from "./hooks/useDocumentKeydown";
 import SearchResult from "./components/SearchResult";
+import SearchInput from "./components/SearchInput"
 
 const extensionSpec = {
   id: "mocjdmglnkelnbnfnklgfgphebdbaopl",
@@ -100,24 +100,6 @@ function Modal({ isOpen, onRequestClose, children }) {
         {children}
       </ReactModal>
     );
-}
-
-function SearchInput() {
-    const focusCallback = useFocusCallback()
-    return (
-        <div className="flexbox flexbox-align-center  padding-left-small padding-right-tiny" style={{ height: 44 }} >
-            <div className="flexbox flexbox-centered padding-left-smaller padding-right-medium shade-087">
-                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" style={{ stroke: "currentColor", strokeWidth: 4, height: 16, width: 16, display: "block" }}>
-                    <g fill="none">
-                        <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
-                    </g>
-                </svg>
-            </div>
-            <div class="flexbox-grow-1">
-                <input type="text" className="shade-087 font-size-medium font-weight-medium line-height-medium overflow-ellipsis no-border no-outline no-box-shadow full-width background-none" ref={focusCallback}/>
-            </div>
-        </div>
-    )
 }
 
 function extensionSpecToEntries({id, name, actions}) {
