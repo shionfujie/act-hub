02ea55071a58f463ece938fb4a774137d9d693b8
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-12 17:46:46 +0900

    request available apis

--- src/background.js
+++ src/background.js
@@ -1,5 +1,11 @@
 /*global chrome*/
 
+chrome.management.onInstalled.addListener(({id}) => {
+  chrome.runtime.sendMessage(id, {type: "action spec"}, spec => {
+    console.debug(spec)
+  })
+})
+
 chrome.runtime.onConnect.addListener(({ name, onMessage }) => {
   if (name == 'ActHub')
     onMessage.addListener(message => {
