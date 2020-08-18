0d402f0984208aed8fe70f7f37f525628befd331
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-18 18:48:55 +0900

    skelton modal

--- src/content.js
+++ src/content.js
@@ -5,6 +5,7 @@ import "./css/content.css";
 import useSwitch from "./hooks/useSwitch";
 import useDocumentKeydown from "./hooks/useDocumentKeydown";
 import SearchModal from "./components/SearchModal";
+import ReactModal from "react-modal";
 
 function Main() {
   const [modalIsOpen, openModal, closeModal] = useSwitch();
@@ -12,14 +13,65 @@ function Main() {
     if (key == "p" && shiftKey && metaKey) openModal();
   });
   return (
-    <SearchModal
-      isOpen={modalIsOpen}
-      onRequestClose={closeModal}
-      onSelectAction={(id, action) => {
-        closeModal()
-        chrome.runtime.sendMessage(id, {type: "execute action", action})
+    <KeyBindingModal isOpen={modalIsOpen} onRequestClose={closeModal} />
+    // <SearchModal
+    //   isOpen={modalIsOpen}
+    //   onRequestClose={closeModal}
+    //   onSelectAction={(id, action) => {
+    //     closeModal();
+    //     chrome.runtime.sendMessage(id, { type: "execute action", action });
+    //   }}
+    // />
+  );
+}
+
+function KeyBindingModal({isOpen, onRequestClose}) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+      <div>Test</div>
+    </Modal>
+  );
+}
+
+function KeyBindingInput() {
+  return <div></div>;
+}
+
+function Modal({ isOpen, onRequestClose, children }) {
+  return (
+    <ReactModal
+      id="act-hub-modal-root"
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={{
+        overlay: {
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          backgroundColor: "rgba(255, 255, 255, .0)",
+          zIndex: 100000
+        },
+        content: {
+          position: null,
+          padding: 0,
+          top: null,
+          right: null,
+          left: null,
+          bottom: null,
+          marginTop: "20vh",
+          marginLeft: 24,
+          marginRight: 24,
+          maxWidth: 400,
+          flexGrow: 1,
+          border: 0,
+          borderRadius: 2,
+          boxShadow: "rgba(29, 17, 17, 0.15) 0px 5px 12px"
+        }
       }}
-    />
+    >
+      {children}
+    </ReactModal>
   );
 }
 
