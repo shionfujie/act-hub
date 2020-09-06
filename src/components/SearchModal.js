b89a7cc517a1a4756d72d96a8db68f5efa1cb5c9
shion.t.fujie <tonaluna32966@gmail.com>
2020-09-06 20:39:26 +0900

    comparison by density and position

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -7,6 +7,8 @@ import containsSparsely from "../util/containsSparsely";
 import useOnKeyDown from "../hooks/useOnKeyDown";
 import useOnKeyUp from "../hooks/useOnKeyUp";
 import combinefuns from "../util/combineFuns";
+import compareCaseInsensitively from "../util/compareCaseInsensitively";
+
 
 export default function SearchModal({
   isOpen,
@@ -101,10 +103,63 @@ const internalActions = [
 ];
 
 function constructEntries(actionSpecs, q) {
-  return [
+  const entries$ = [
     ...internalActions,
     ...actionSpecs.flatMap(extensionSpecToEntries)
-  ].filter(matchQuery(q))
+  ]
+  console.debug(entries$)
+  console.debug(q)
+  const r = []
+  const entries = []
+  for (const entry of entries$) {
+    const m = matchResult(entry, q)
+    console.debug(m)
+    if (m.count >= q.length) {
+      if (r.length == 0) {
+        r.push(m)
+        entries.push(m.entry)
+      }
+      else {
+        const lt = (m, m1) => {
+          if (m.count > m1.count) return true
+          else if (m.count < m1.count) return false
+          else {
+            if (m.density < m1.density) return true
+            else if (m.density > m1.density) return false
+            else {
+              if(m.position < m1.position) return true
+              else if (m.position > m1.position) return false
+              else return false
+            }
+          }
+        }
+        for (var j = r.length - 1; j >= 0 && lt(m, r[j]); j--);
+        r.splice(j + 1, 0, m)
+        entries.splice(j + 1, 0, m.entry)
+      }
+    }
+  }
+  console.debug(r)
+  return entries
+}
+
+function matchResult(entry, q) {
+  const ec = Array.from(entry.title)
+  const qc = Array.from(q)
+  var m
+  var position = - 1
+  var density = 0
+  var count = 0
+  for (var i = 0, j = 0; i < ec.length && j < qc.length; i++) {
+    console.log()
+    if (compareCaseInsensitively(ec[i], qc[j])) {
+      if (j === 0) position = i
+      else density += i - m
+      m = i
+      j++; count++
+    }
+  }
+  return {entry, position, density, count}
 }
 
 const matchQuery = q => entry =>
