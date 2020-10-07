4e0f261a3ef046a32e2b9e7d815ea839b44ef12d
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-07 11:30:14 +0900

    action utilities ramify

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import ReactModal from "react-modal";
 import SearchableList from "./SearchableList";
-import compareCaseInsensitively from "../util/compareCaseInsensitively";
+import { extensionSpecToEntries, sortActions } from "../util/actions";
 
 
 export default function SearchModal({
@@ -38,6 +38,10 @@ function useActions() {
   return [actions, setQuery];
 }
 
+function getActionSpecs(callback) {
+  chrome.storage.sync.get({ actionSpecs: [] }, ({ actionSpecs }) => callback(actionSpecs))
+}
+
 const internalActions = [
   {
     key: "internal-0",
@@ -46,67 +50,6 @@ const internalActions = [
     action: { type: "keybinding" }
   }
 ];
-
-function getActionSpecs(callback) {
-  chrome.storage.sync.get({ actionSpecs: [] }, ({ actionSpecs }) => callback(actionSpecs))
-}
-
-function extensionSpecToEntries({ id, name, actions }) {
-  return actions.map((action, index) => {
-    return {
-      key: `${id}-${index}`,
-      extensionId: id,
-      title: action.displayName || `${name}: ${action.name}`,
-      action
-    };
-  });
-}
-
-function sortActions(actions, q) {
-  const ms = []
-  const sorted = []
-  for (const action of actions) {
-    const m = match(action.title, q)
-    if (m.count >= q.length) {
-      for (var j = ms.length - 1; j >= 0 && lt(m, ms[j]); j--);
-      ms.splice(j + 1, 0, m)
-      sorted.splice(j + 1, 0, action)
-    }
-  }
-  return sorted
-}
-
-function match(title, q) {
-  const ec = Array.from(title)
-  const qc = Array.from(q)
-  var m
-  var position = - 1
-  var density = 0
-  var count = 0
-  for (var i = 0, j = 0; i < ec.length && j < qc.length; i++) {
-    if (compareCaseInsensitively(ec[i], qc[j])) {
-      if (j === 0) position = i
-      else density += i - m
-      m = i
-      j++; count++
-    }
-  }
-  return {position, density, count}
-}
-
-const lt = (m, m1) => {
-  if (m.count > m1.count) return true
-  else if (m.count < m1.count) return false
-  else {
-    if (m.density < m1.density) return true
-    else if (m.density > m1.density) return false
-    else {
-      if(m.position < m1.position) return true
-      else if (m.position > m1.position) return false
-      else return false
-    }
-  }
-}
 
 function Modal({ isOpen, onRequestClose, children }) {
   return (
---
+++ src/util/actions.js
@@ -0,0 +1,59 @@
+import compareCaseInsensitively from "../util/compareCaseInsensitively";
+
+export function extensionSpecToEntries({ id, name, actions }) {
+  return actions.map((action, index) => {
+    return {
+      key: `${id}-${index}`,
+      extensionId: id,
+      title: action.displayName || `${name}: ${action.name}`,
+      action
+    };
+  });
+}
+
+export function sortActions(actions, q) {
+  const ms = [];
+  const sorted = [];
+  for (const action of actions) {
+    const m = match(action.title, q);
+    if (m.count >= q.length) {
+      for (var j = ms.length - 1; j >= 0 && lt(m, ms[j]); j--);
+      ms.splice(j + 1, 0, m);
+      sorted.splice(j + 1, 0, action);
+    }
+  }
+  return sorted;
+}
+
+function match(title, q) {
+  const ec = Array.from(title);
+  const qc = Array.from(q);
+  var m;
+  var position = -1;
+  var density = 0;
+  var count = 0;
+  for (var i = 0, j = 0; i < ec.length && j < qc.length; i++) {
+    if (compareCaseInsensitively(ec[i], qc[j])) {
+      if (j === 0) position = i;
+      else density += i - m;
+      m = i;
+      j++;
+      count++;
+    }
+  }
+  return { position, density, count };
+}
+
+const lt = (m, m1) => {
+  if (m.count > m1.count) return true;
+  else if (m.count < m1.count) return false;
+  else {
+    if (m.density < m1.density) return true;
+    else if (m.density > m1.density) return false;
+    else {
+      if (m.position < m1.position) return true;
+      else if (m.position > m1.position) return false;
+      else return false;
+    }
+  }
+};
