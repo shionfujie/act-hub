511c466f8a761be194f55b48c6d5670647380079
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-09 17:50:26 +0900

    unused imports

--- src/components/SearchableEntryList.js
+++ src/components/SearchableEntryList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import SearchInput from "./SearchInput";
 import SearchEntryList from "./SearchEntryList";
 import useOnKeyDown from "../hooks/useOnKeyDown";
