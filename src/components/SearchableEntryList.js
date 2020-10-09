161df172d2488ead6e1f7e2a63b78ad9b90e3c1f
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-09 16:42:37 +0900

    search controller and selection controller

--- src/components/SearchableEntryList.js
+++ src/components/SearchableEntryList.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
-import SelectableList from "./SelectableEntryList";
 import SearchInput from "./SearchInput";
+import SearchEntryList from "./SearchEntryList";
 import { searchEntries } from "../util/actions";
+import useOnKeyDown from "../hooks/useOnKeyDown";
+import useOnKeyUp from "../hooks/useOnKeyUp";
+import combinefuns from "../util/combineFuns";
 
 export default function SearchableEntryList({
   isLoading,
@@ -9,26 +12,95 @@ export default function SearchableEntryList({
   onRequestClose,
   onSelectEntry
 }) {
-  const [filtered, setFiltered] = useState(entries);
-  const [q, setQuery] = useState("");
-  useEffect(() => {
-    if (isLoading) return;
-    setFiltered(searchEntries(entries, q));
-  }, [q, entries, isLoading]);
+  const searchController = useSearchController(entries, isLoading);
+  const selectionController = useSelectionController(
+    searchController.searchResult,
+    ({ extensionId, action }) => {
+      onSelectEntry(extensionId, action);
+      searchController.setQuery("");
+    }
+  );
+  const [keyUpEvent, onkeyupRef] = useKeyUpEvent();
+  const [keyDownEvent, onkeydownRef] = useKeyDownEvent();
+  if (keyUpEvent) {
+    keyUpEvent.stopPropagation();
+    const key = keyUpEvent.key;
+    if (key === "ArrowUp") selectionController.shiftSelection(-1);
+    else if (key === "ArrowDown") selectionController.shiftSelection(1);
+  }
+  if (keyDownEvent) {
+    keyDownEvent.stopPropagation();
+    const key = keyDownEvent.key;
+    if (key === "Enter") selectionController.submitEntry();
+    else if (key == "Escape") onRequestClose();
+  }
   return (
     <div className="flexbox flexbox-direction-column flexbox-grow-1 radius-small border-width-normal border-solid border-color-shade-013 background-shade-003 overflow-hidden">
-      <SearchInput onChange={setQuery} />
-      <SelectableList
-        entries={filtered}
-        onSelectEntry={(extensionId, action) => {
-          onSelectEntry(extensionId, action);
-          setQuery("");
-        }}
-        onRequestClose={() => {
-          onRequestClose();
-          setQuery("");
-        }}
-      />
+      <div ref={combinefuns(onkeydownRef, onkeyupRef)}>
+        <SearchInput onChange={searchController.setQuery} />
+      </div>
+      {searchController.searchResult.length > 0 && (
+        <SearchEntryList
+          entries={searchController.searchResult}
+          selectedIndex={selectionController.selectedIndex}
+          submitEntry={selectionController.submitEntry}
+          selectIndex={selectionController.selectIndex}
+        />
+      )}
     </div>
   );
+}
+
+function useSearchController(entries, isLoading) {
+  const [q, setQuery] = useState("");
+  const [controller, setController] = useState({
+    searchResult: entries,
+    setQuery: setQuery
+  });
+  useEffect(() => {
+    if (isLoading) return;
+    setController({ ...controller, searchResult: searchEntries(entries, q) });
+  }, [q, entries, isLoading]);
+  return controller;
+}
+
+function useSelectionController(entries, onSelectEntry) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  function selectIndex(index) {
+    if (-1 < index && index < entries.length) setSelectedIndex(index);
+  }
+  function shiftSelection(offset) {
+    selectIndex(selectedIndex + offset);
+  }
+  function submitEntry() {
+    onSelectEntry(entries[selectedIndex]);
+  }
+  useEffect(() => {
+    selectIndex(0);
+  }, [entries]);
+  return {
+    entries,
+    selectedIndex,
+    selectIndex,
+    shiftSelection,
+    submitEntry
+  };
+}
+
+function useKeyUpEvent() {
+  const [event, setEvent] = useState(null);
+  const ref = useOnKeyUp(event => {
+    setEvent(event);
+    setEvent(null);
+  });
+  return [event, ref];
+}
+
+function useKeyDownEvent() {
+  const [event, setEvent] = useState(null);
+  const ref = useOnKeyDown(event => {
+    setEvent(event);
+    setEvent(null);
+  });
+  return [event, ref];
 }
